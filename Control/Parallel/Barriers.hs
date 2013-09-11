32ca8ef50bb9a46a9e5078aa0520912a0e3edd82
Johannes Röhl <joroehl@gmx.de>
2013-09-11 15:08:54 +0200

    Barriers: some minor haddock fixes

--- Control/Parallel/Barriers.hs
+++ Control/Parallel/Barriers.hs
@@ -10,7 +10,7 @@ import System.IO.Unsafe (unsafePerformIO)
 import GHC.Conc.Sync (runSparks, yield)
 
 {- |
-@ x `parBarrier` y @ will spark the evaluation of @x@, flush the spark pool,
+@ x \`parBarrier\` y @ will spark the evaluation of @x@, flush the spark pool,
 /and then/ return y.
 -}
 infixr 0 `parBarrier`
@@ -36,20 +36,20 @@ Conceptually similar to 'pseq'. The first argument is evaluated to weak head
 normal form, waits for sparks to be run and then return the second argument.
 This is usefull in cases like:
 
-> x0 `'par'` x1 `'par'` ... `'par'` xn `'pseq'` sequentialCombinerOfX1toXn
+> x0 `par` x1 `par` ... `par` xn `pseq` sequentialCombinerOfX1toXn
 
-might generates many "fizzled" sparks, if sequentialCombinerOfX1toXn uses the
+might generates many \"fizzled\" sparks, if sequentialCombinerOfX1toXn uses the
 xs faster than the sparks are run. Changing this to
 
-> x0 `'par'` x1 `'par'` ... `'par'` xn `pseqBarrier` sequentialCombinerOfX1toXn
+> x0 `par` x1 `par` ... `par` xn `pseqBarrier` sequentialCombinerOfX1toXn
 
-will avoid the "fizzling".
+will avoid the \"fizzling\".
 -}
 infixr 0 `pseqBarrier`
 pseqBarrier :: a -> b -> b
 x `pseqBarrier` y = x `pseq` waitForSparks y
 
--- | @forceParallel x@ is the same as @x `'pseqBarrier'` x@. It is useful, if
+-- | @forceParallel x@ is the same as @x \`pseqBarrier\` x@. It is useful, if
 -- the evaluation of @x@ sparks parallel evaluations and one wants to make
 -- sure, that all sparks are run before @x@ is used. In other cases it is an
 -- inefficient variant of 'id'.
