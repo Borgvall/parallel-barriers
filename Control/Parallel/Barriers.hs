9ac8febc098b3d8392120eb265c343dc50113839
Johannes Röhl <joroehl@gmx.de>
2013-09-13 15:30:39 +0200

    Barriers: utilities for foldable structures

--- Control/Parallel/Barriers.hs
+++ Control/Parallel/Barriers.hs
@@ -1,11 +1,15 @@
 module Control.Parallel.Barriers (
   -- * Parallel Helper Functions
   parBarrier, pseqBarrier, forceParallel,
+  -- * Utilities for Foldable Structures
+  forceParList, forceParFoldable,
   -- * Flush the Spark Pool
   flushSparkPool
 ) where
 
 import Control.Parallel
+import Data.Foldable (Foldable)
+import qualified Data.Foldable as F
 import System.IO.Unsafe (unsafePerformIO)
 import GHC.Conc.Sync (runSparks, yield)
 
@@ -55,3 +59,31 @@ x `pseqBarrier` y = x `pseq` waitForSparks y
 -- inefficient variant of 'id'.
 forceParallel :: a -> a
 forceParallel x = x `pseqBarrier` x
+
+{- |
+Evaluate the elements of a list in parallel. See 'forceParFoldable' for more details.
+-}
+forceParList :: [a] -> [a]
+forceParList xs = forceParallel $ foldr par xs xs
+
+{- |
+Evaluate the elements of a 'Foldable' structure in parallel. It is typically
+used by changing @xs = ...@ to @ xs = 'forceParFoldable' $ ... @. The parallel
+evaluation happens, when @xs@ is used for the first time. If @xs@ gets inlined
+and used multiple times, the evaluation will be performed multiple times too.
+
+'forceParFoldable' will /not/ copy the structure and evaluates the elements to
+weak head normal form. If evaluation to normal form is needed, one might use
+something like:
+
+@ forceParDeep = 'forceParFoldable' . 'fmap' 'Control.DeepSeq.force' @
+
+'forceParFoldable' adds strictness. In particular:
+
+ 1. If @xs@ is infinite, @ 'forceParFoldable' xs @ is bottom.
+
+ 2. If @xs@ contains bottom as element, @ 'forceParFoldable' xs @ might be
+ bottom (non deterministic).
+-}
+forceParFoldable :: Foldable t => t a -> t a
+forceParFoldable xs = forceParallel $ F.foldr par xs xs
